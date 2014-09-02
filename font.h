27d4a50ffd0703872381043aac0b104089310447
Oliver Dippel <o.dippel@gmx.de>
2014-09-02 13:37:42 +0200

    adding font.c .h to git

---
+++ font.h
@@ -0,0 +1,7 @@
+
+float output_char_gl (char c, float x, float y, float z, float s);
+float output_charlen_gl (char c, float s);
+void output_text_gl_center (char *text, float x, float y, float z, float s);
+void output_text_gl (char *text, float x, float y, float z, float s);
+void output_text_dxf (char *text, float x, float y, float z, float s);
+
